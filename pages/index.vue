9fd608c3f72ef688b696d47aa808ec3addda32e0
knicKnack-Mark <markneilbertiz21@gmail.com>
2025-02-21 23:58:16 +0800

    feat: update carousel implementation and enhance resort description on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,48 @@
 <template>
   <div>
- 
-    <div id="carouselExampleIndicators" class="carousel slide " data-bs-ride="carousel">
-    <ol class="carousel-indicators">
-      <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>
-      <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
-      <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img class="d-block w-100" src="https://i.postimg.cc/bNQp0RDW/1.jpg" alt="First slide">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Slider One Item</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, nulla, tempore. Deserunt excepturi quas vero.</p>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="https://i.postimg.cc/pVzg3LWn/2.jpg" alt="Second slide">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Slider two Item</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, nulla, tempore. Deserunt excepturi quas vero.</p>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="https://i.postimg.cc/0y2F6Gpp/3.jpg" alt="Third slide">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Slider three Item</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, nulla, tempore. Deserunt excepturi quas vero.</p>
-        </div>
-      </div>
+    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <NuxtImg src="resort.jpg" class="d-block w-100" alt="..."/>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </a>
+    <div class="carousel-item">
+      <NuxtImg src="resort1.jpg" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <NuxtImg src="resort2.jpg" class="d-block w-100" alt="..."/>
+    </div>
   </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    
+<div class="text-center mt-5 container px-md-10 ">
+  <p class="font-tnr fs-2 text-secondary">Janns Spring Resort</p>
+  <hr class="mx-auto border-2" width="40%" />
+  <p class="font-carattere fs-2 text-secondary">Embrace the calm of nature</p>
+  <p class="custom-padding-about">Tucked away in the scenic town of Malilipot, Albay, along the Bypass Road in Barangay 1, Jann’s Spring Resort is a hidden paradise where nature’s beauty meets relaxation. This natural spring resort is the perfect getaway for families, friends, and travelers looking to unwind and reconnect with nature.
+
+At the heart of our resort is a large, crystal-clear spring pool, fed by pure, refreshing waters straight from nature. Whether you're floating under the sun or taking a rejuvenating dip, our pool offers an invigorating experience that soothes the body and soul.
+
+For those seeking a peaceful retreat or a venue for special occasions, our spacious cabin with a hall provides a cozy and inviting space for gatherings, celebrations, or even quiet moments of relaxation. Surrounded by lush greenery and the soothing sounds of flowing water, Jann’s Spring Resort offers a serene and refreshing ambiance, making it the perfect escape from the hustle and bustle of daily life.
+
+Come and experience the magic of nature, the comfort of a relaxing retreat, and the joy of making beautiful memories. Whether it’s a day of fun, a weekend getaway, or a special celebration, Jann’s Spring Resort welcomes you to unwind, recharge, and enjoy the beauty of the springs.</p>
+</div>
+
+
+
   </div>
 </template>
 
 <script setup>
 
 
-const label = 'lol'; // Define the button text
+
 </script>
 
