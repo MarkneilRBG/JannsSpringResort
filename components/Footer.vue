40a781c9029976e6149729826e5665fb95f64549
knicKnack-Mark <markneilbertiz21@gmail.com>
2025-02-22 23:00:39 +0800

    feat: add footer component and update header branding to Janns

---
+++ components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="text-center bg-body-tertiary mt-5">
+  <!-- Grid container -->
+  <div class="container pt-4">
+    <!-- Section: Social media -->
+    <div class="d-flex justify-content-center pb-3">
+                <NuxtLink class="mx-4" to="#"><Icon name="logos:facebook" size="1.5em" /></NuxtLink>
+                <NuxtLink class="mx-4" to="#"><Icon name="flat-color-icons:google" size="1.5em" /></NuxtLink>
+                <NuxtLink class="mx-4 text-dark" to="#"><Icon name="fa6-brands:square-x-twitter" size="1.5em" /></NuxtLink>
+                <NuxtLink class="mx-4" to="#"><Icon name="skill-icons:instagram" size="1.5em" /></NuxtLink>
+                <NuxtLink class="mx-4" to="#"><Icon name="mdi:twitter" size="1.5em" /></NuxtLink>
+              </div>
+    <!-- Section: Social media -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2020 Copyright:
+    <a class="text-body" href="https://mdbootstrap.com/">Janns Spring Resort</a>
+  </div>
+  <!-- Copyright -->
+  </footer>
+</template>
--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light fixed-top text-primary">
     <div class="container d-flex justify-content-between">
-      <NuxtLink class="navbar-brand" href="#">Mouri</NuxtLink>
+      <NuxtLink class="navbar-brand" to="#">Janns</NuxtLink>
       <button
         class="navbar-toggler"
         type="button"
@@ -17,19 +17,19 @@
       <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
         <ul class="navbar-nav">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home</a>
+            <NuxtLink class="nav-link" to="#">Home</NuxtLink>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
+            <NuxtLink class="nav-link" to="#">About</NuxtLink>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Portfolio</a>
+            <NuxtLink class="nav-link" to="#">Portfolio</NuxtLink>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Services</a>
+            <NuxtLink class="nav-link" to="#">Services</NuxtLink>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
+            <NuxtLink class="nav-link" to="#">Contact</NuxtLink>
           </li>
         </ul>
       </div>
