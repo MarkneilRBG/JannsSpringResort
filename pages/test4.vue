828416c96bafb7c02883c9e95bfe32ce2c51a94c
knicKnack-Mark <markneilbertiz21@gmail.com>
2025-02-25 22:29:45 +0800

    feat: integrate Flatpickr component and update testimonials display in test4 page

--- pages/test4.vue
+++ pages/test4.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="container">
-    <MultiCarousel />
+  <div>
+    <!-- <h1>Select a Date</h1>
+    <Flatpickr v-model="selectedDate" :config="flatpickrConfig" />
+    <p>Selected Date: {{ selectedDate }}</p> -->
+
+    <Testimonials />
   </div>
 </template>
+
+<script setup>
+definePageMeta({
+  layout: false
+});
+
+// import { ref } from 'vue'
+// import Flatpickr from 'vue-flatpickr-component'
+// import 'flatpickr/dist/flatpickr.css'
+
+// const selectedDate = ref(null)
+
+// // Configure flatpickr options
+// const flatpickrConfig = {
+//   altInput: true,
+//   altFormat: 'F j, Y',
+//   dateFormat: 'Y-m-d',
+// }
+</script>
