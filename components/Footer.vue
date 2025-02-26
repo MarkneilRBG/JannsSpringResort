8969eb60fa65d98f7a9e4a92d87e222465d4d3ca
knicKnack-Mark <markneilbertiz21@gmail.com>
2025-02-26 22:21:19 +0800

    feat: update Footer component to include NuxtLink for Privacy Policy and Terms of Use

--- components/Footer.vue
+++ components/Footer.vue
@@ -18,7 +18,7 @@
  
   <div class="text-center p-3 font-size-footer" style="background-color: rgba(0, 0, 0, 0.05);">
     <p class="fs-5">
-    Privacy Policy | Terms of Use 
+    <NuxtLink class="text-decoration-none text-black" to="#"> Privacy Policy  </NuxtLink> | <NuxtLink class="text-decoration-none text-black" to="#">Terms of Use </NuxtLink>
    </p>
     © 2025 Copyright:
     <a class="text-body" href="https://mdbootstrap.com/">Janns Spring Resort.</a>
