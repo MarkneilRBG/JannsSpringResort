5fc345108a7850a385c82cac430c57c48ce3e558
knicKnack-Mark <markneilbertiz21@gmail.com>
2025-02-26 22:17:54 +0800

    feat: add BaseIcon component for reusable icon rendering

---
+++ components/BaseIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Icon :name="name" :size="size" :color="color" />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: String,
+    default: "1.5em",
+  },
+  color: {
+    type: String,
+    default: "currentColor",
+  },
+});
+</script>
