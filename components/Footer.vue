9818c2c48eb57895f6443842764fa4eac10d46fd
knicKnack-Mark <markneilbertiz21@gmail.com>
2025-02-27 18:48:32 +0800

    feat: update Footer component to enhance link styling for Privacy Policy and Terms of Use

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,7 +5,7 @@
     <!-- Section: Social media -->
     <div class="d-flex justify-content-center pb-3">
                 <NuxtLink class="mx-4" to="https://www.facebook.com/jannspringresort"><Icon name="logos:facebook" size="1.5em" /></NuxtLink>
-                <NuxtLink class="mx-4" to="#"><Icon name="flat-color-icons:google" size="1.5em" /></NuxtLink>
+                <!-- <NuxtLink class="mx-4" to="#"><Icon name="flat-color-icons:google" size="1.5em" /></NuxtLink> -->
                 <NuxtLink class="mx-4 text-dark" to="#"><Icon name="fa6-brands:square-x-twitter" size="1.5em" /></NuxtLink>
                 <NuxtLink class="mx-4" to="https://www.instagram.com/explore/locations/118209244609625/janns-spring-resort/"><Icon name="skill-icons:instagram" size="1.5em" /></NuxtLink>
                 <NuxtLink class="mx-4" to="#"><Icon name="mdi:twitter" size="1.5em" /></NuxtLink>
@@ -18,7 +18,9 @@
  
   <div class="text-center p-3 font-size-footer" style="background-color: rgba(0, 0, 0, 0.05);">
     <p class="fs-5">
-    <NuxtLink class="text-decoration-none text-black" to="privacy"> Privacy Policy  </NuxtLink> | <NuxtLink class="text-decoration-none text-black" to="terms">Terms of Use </NuxtLink>
+    <NuxtLink class="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" to="privacy"> Privacy Policy  </NuxtLink> 
+    | 
+    <NuxtLink class="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" to="terms">Terms of Use </NuxtLink>
    </p>
     © 2025 Copyright:
     <a class="text-body" href="https://mdbootstrap.com/">Janns Spring Resort.</a>
