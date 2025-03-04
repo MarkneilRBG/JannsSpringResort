49e3bad050802ca5f8c9bb732e5db7c59a041bf9
knicKnack-Mark <markneilbertiz21@gmail.com>
2025-03-04 22:31:33 +0800

    feat: update services page layout with header and padding adjustments

--- pages/services.vue
+++ pages/services.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    this is services
+  <div class="pt-6 pt-md-2 pt-lg-6">
+    <h1 class="fs-3 text-center">Our Services</h1>
+    <div>
+      
+    </div>
   </div>
 </template>
